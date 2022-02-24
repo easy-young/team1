03db5658d2f533f86097ffeb2736fb81208c6bb3
rlatjdgh1 <rlatjdgh981002@gmail.com>
2022-02-25 04:59:35 +0900

    DB board 테이블 타임스탬프  수정하고 필드 추가함, /views/admin 아래 board, board_delete에 css 적용했음.

--- DB schema/db.sql
+++ DB schema/db.sql
@@ -17,19 +17,10 @@ CREATE TABLE user(
     status varchar(30) default 'Available' 
     );
 
-    -- INSERT INTO user(userid,userpw,name,username,birth,gender,contact,phone)
-    -- VALUES('shkim','1111','김성호','john','1998-10-02','agender','02-1111-2222','010-3333-4444');
-
-    -- INSERT INTO user(userid,userpw,name,username,birth,gender,contact,phone)
-    -- VALUES('cslee','1111','이철수','charles','2000-12-25','male','02-4444-5555','010-6666-7777');
-
-    -- INSERT INTO user(userid,userpw,name,username,birth,gender,contact,phone) 
-    -- VALUES('yhpark','1111','박영희','jane','2100-01-18','female','02-8888-9999','010-1234-5678');
-    
 CREATE TABLE board(
   idx int auto_increment primary key,
   title varchar(40) not null,
   content text not null,
-  date timestamp not null,
+  date timestamp default current_timestamp,
   hit int not null
 );
