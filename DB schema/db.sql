fb930136e97614ac5b9a3a20e6e0222b3be9eb3c
kimhelp <operat8009@gmail.com>
2022-02-25 11:15:32 +0900

    회원가입 후 index로 돌아가기

--- DB schema/db.sql
+++ DB schema/db.sql
@@ -17,26 +17,10 @@ CREATE TABLE user(
     status varchar(30) default 'Available' 
     );
 
-<<<<<<< HEAD
-    -- INSERT INTO user(userid,userpw,name,username,birth,gender,contact,phone)
-    -- VALUES('shkim','1111','김성호','john','1998-10-02','agender','02-1111-2222','010-3333-4444');
-
-    -- INSERT INTO user(userid,userpw,name,username,birth,gender,contact,phone)
-    -- VALUES('cslee','1111','이철수','charles','2000-12-25','male','02-4444-5555','010-6666-7777');
-
-    -- INSERT INTO user(userid,userpw,name,username,birth,gender,contact,phone) 
-    -- VALUES('yhpark','1111','박영희','jane','2100-01-18','female','02-8888-9999','010-1234-5678');
-    
-=======
->>>>>>> d757b5bd85b5724d5ce4c86733c1a0f74ccfb267
 CREATE TABLE board(
   idx int auto_increment primary key,
   title varchar(40) not null,
   content text not null,
-<<<<<<< HEAD
-  date timestamp not null,
-=======
   date timestamp default current_timestamp,
->>>>>>> d757b5bd85b5724d5ce4c86733c1a0f74ccfb267
   hit int not null
 );
